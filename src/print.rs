016b1ee4be180e83fd31c857644c58e240c5e591
jollySleeper <git@pm21.anonaddy.com>
2024-01-12 10:02:46 +0530

    Updated: Print: Better Handles Errors Now

--- src/print.rs
+++ src/print.rs
@@ -5,12 +5,15 @@
 use std::io;
 use std::io::Write;
 use termion::{color, style};
+use crate::error::QuotermError;
 
-fn flush_output() {
-    io::stdout().flush().unwrap();
+fn flush_output() -> Result<(), QuotermError> {
+    io::stdout()
+        .flush()
+        .map_err(|e| QuotermError::IoError(e))
 }
 
-pub fn print_colored_message<C: color::Color>(message: &str, color: color::Fg<C>) {
+pub fn print_colored_message<C: color::Color>(message: &str, color: color::Fg<C>) -> Result<(), QuotermError> {
     println!("{}{}{}", color, message, style::Reset);
     flush_output();
 }
@@ -19,17 +22,18 @@ pub fn print_colored_message_with_padding<C: color::Color>(
     padding: usize,
     message: &str,
     color: color::Fg<C>,
-) {
+) -> Result<(), QuotermError> {
     println!("{:padding$}{}{}{}", "", color, message, style::Reset);
     flush_output();
 }
 
-fn set_bold_style() {
+fn set_bold_style() -> Result<(), QuotermError> {
     print!("{}", style::Bold);
+    flush_output();
 }
 
-pub fn print_colored_message_in_bold<C: color::Color>(message: &str, color: color::Fg<C>) {
-    set_bold_style();
+pub fn print_colored_message_in_bold<C: color::Color>(message: &str, color: color::Fg<C>) -> Result<(), QuotermError> {
+    set_bold_style()?;
     print_colored_message(message, color);
 }
 
@@ -37,7 +41,7 @@ pub fn print_colored_message_with_padding_in_bold<C: color::Color>(
     padding: usize,
     message: &str,
     color: color::Fg<C>,
-) {
-    set_bold_style();
+) -> Result<(), QuotermError> {
+    set_bold_style()?;
     print_colored_message_with_padding(padding, message, color);
 }
