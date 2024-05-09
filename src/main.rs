96348988feff590e8e16cc750ff29cbd72843b71
jollySleeper <git@pm21.anonaddy.com>
2024-05-09 09:51:21 +0530

    Added: Feature: Printing Random Quote

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rand::Rng;
+use serde::{Deserialize, Serialize};
+
+pub mod quotes;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Quote {
+    content: String,
+    author: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // Reading JSON File
+    let json_file: Vec<Quote> = serde_json::from_str(&quotes::get_quotes()).unwrap();
+
+    let entries: usize = json_file.len();
+    let mut rng = rand::thread_rng();
+    let index: usize = rng.gen_range(0..entries);
+
+    let random_quote: &Quote = match json_file.get(index) {
+        Some(_x) => json_file.get(index).unwrap(),
+        None => panic!("woops"),
+    };
+
+    println!("{:?}", random_quote);
 }
